9d6236f2c457bf7f743539c9ea16be76f12827be
durck <poiulkjhmnv50@gmail.com>
2019-10-14 22:48:23 +0300

    changed service's class

--- File_copier_service/Service1.cs
+++ File_copier_service/Service1.cs
@@ -5,22 +5,104 @@ using System.Data;
 using System.Diagnostics;
 using System.ServiceProcess;
 using System.Text;
+using System.IO;
+using System.Threading;
 
 namespace File_copier_service
 {
     public partial class Service1 : ServiceBase
     {
+        Logger logger;
         public Service1()
         {
             InitializeComponent();
+            this.CanStop = true;
+            this.CanPauseAndContinue = true;
+            this.AutoLog = true;
         }
 
         protected override void OnStart(string[] args)
         {
+            logger = new Logger();
+            Thread loggerThread = new Thread(new ThreadStart(logger.Start));
+            loggerThread.Start();
         }
 
         protected override void OnStop()
         {
+            logger.Stop();
+            Thread.Sleep(1000);
+        }
+    }
+
+    class Logger
+    {
+        FileSystemWatcher watcher;
+        object obj = new object();
+        bool enabled = true;
+        
+        public Logger ()
+        {
+            watcher = new FileSystemWatcher("D:\\Temp");
+            watcher.Deleted += Watcher_Deleted;
+            watcher.Created += Watcher_Created;
+            watcher.Changed += Watcher_Changed;
+            watcher.Renamed += Watcher_Renamed;
+        }
+
+        public void Start()
+        {
+            watcher.EnableRaisingEvents = true;
+            while (enabled)
+            {
+                Thread.Sleep(1000);
+            }
+        }
+
+        public void Stop()
+        {
+            watcher.EnableRaisingEvents = false;
+            enabled = false;
+        }
+
+        private void Watcher_Renamed(object sender, RenamedEventArgs e)
+        {
+            string fileEvent = "переименован в " + e.FullPath;
+            string filePath = e.OldFullPath;
+            RecordEntry(fileEvent, filePath);
+        }
+
+        private void Watcher_Created(object sender, FileSystemEventArgs e)
+        {
+            string fileEvent = "создан";
+            string filePath = e.FullPath;
+            RecordEntry(fileEvent, filePath);
+        }
+
+        private void Watcher_Changed(object sender, FileSystemEventArgs e)
+        {
+            string fileEvent = "изменён";
+            string filePath = e.FullPath;
+            RecordEntry(fileEvent, filePath);
+        }
+
+        private void Watcher_Deleted(object sender, FileSystemEventArgs e)
+        {
+            string fileEvent = "удалён";
+            string filePath = e.FullPath;
+            RecordEntry(fileEvent, filePath);
+        }
+
+        private void RecordEntry(string fileEvent, string filePath)
+        {
+            lock (obj)
+            {
+                using (StreamWriter writer = new StreamWriter("D:\\templog.txt", true))
+                {
+                    writer.WriteLine(String.Format("{0} файл {1} был {2}", DateTime.Now.ToString("dd/MM/yyyy hh:mm:ss"), filePath, fileEvent));
+                    writer.Flush();
+                }
+            }
         }
     }
 }
