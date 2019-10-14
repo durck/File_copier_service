652daae977e29e00d0f4f0b4266794fc7984348a
durck <poiulkjhmnv50@gmail.com>
2019-10-14 22:57:10 +0300

    created installer

---
+++ File_copier_service/Installer1.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel;
+using System.Configuration.Install;
+using System.ServiceProcess;
+
+namespace File_copier_service
+{
+    [RunInstaller(true)]
+    public partial class Installer1 : Installer
+    {
+        ServiceInstaller serviceInstaller;
+        ServiceProcessInstaller processInstaller;
+
+        public Installer1()
+        {
+            InitializeComponent();
+            serviceInstaller = new ServiceInstaller();
+            processInstaller = new ServiceProcessInstaller();
+
+            processInstaller.Account = ServiceAccount.LocalSystem;
+            serviceInstaller.StartType = ServiceStartMode.Manual;
+            serviceInstaller.ServiceName = "TestService";
+            Installers.Add(processInstaller);
+            Installers.Add(serviceInstaller);
+        }
+    }
+}
--- File_copier_service/Service1.cs
+++ File_copier_service/Service1.cs
@@ -1,10 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Diagnostics;
 using System.ServiceProcess;
-using System.Text;
 using System.IO;
 using System.Threading;
 
